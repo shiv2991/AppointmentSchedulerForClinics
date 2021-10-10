90862a13252735e36cfe6b3b18d919fd5f661b9c
shiv2991 <shiv.2991@gmail.com>
2021-10-11 04:18:13 +0530

    11102021-Adding Roles for account access - hacktoberfest-accepted

---
+++ AppScheduler/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationRoleManager _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccountController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager,
+            ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            RoleManager = roleManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get { return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>(); }
+            private set { _signInManager = value; }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            private set { _userManager = value; }
+        }
+    }
+}
