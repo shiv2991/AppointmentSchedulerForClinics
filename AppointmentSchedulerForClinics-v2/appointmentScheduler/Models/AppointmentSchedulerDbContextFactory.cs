f4fbdb603fc471bb1aa65ffcfe5dd16e12be41bb
shiv2991 <shiv.2991@gmail.com>
2025-10-07 03:44:06 +1030

    Setup EF Core, code-first model, and SQL Server connection

---
+++ AppointmentSchedulerForClinics-v2/appointmentScheduler/Models/AppointmentSchedulerDbContextFactory.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace appointmentScheduler.Data
+{
+    public class AppointmentSchedulerDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
