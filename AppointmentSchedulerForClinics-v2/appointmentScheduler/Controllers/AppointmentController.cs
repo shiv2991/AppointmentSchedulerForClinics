cc1e313dad1089f7e3e94caa1bf198317159fdb4
shiv2991 <shiv.2991@gmail.com>
2025-10-07 04:14:12 +1030

    Scaffold Aspire AppHost project

---
+++ AppointmentSchedulerForClinics-v2/appointmentScheduler/Controllers/AppointmentController.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using appointmentScheduler.Data;
+using appointmentScheduler.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace appointmentScheduler.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
+        {
+            return await _context.Appointments.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+                return NotFound();
+            return appointment;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Appointment>> CreateAppointment(Appointment appointment)
+        {
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAppointment), new { id = appointment.Id }, appointment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAppointment(int id, Appointment appointment)
+        {
+            if (id != appointment.Id)
+                return BadRequest();
+            _context.Entry(appointment).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Appointments.Any(e => e.Id == id))
+                    return NotFound();
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+                return NotFound();
+            _context.Appointments.Remove(appointment);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
